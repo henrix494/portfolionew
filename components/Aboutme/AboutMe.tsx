6994321c44d6fad7ca75fb99942a0292d3ad6ac7
natan <henrix494@gmail.com>
2024-08-22 17:15:42 +0300

    testing animations

--- components/Aboutme/AboutMe.tsx
+++ components/Aboutme/AboutMe.tsx
@@ -8,10 +8,10 @@ export default function AboutMe({ dictionary }: { dictionary: dictionary }) {
       {dictionary.aboutMe.title}
       <div className="lg:w-[950px]  bg-[#0b0f11] mt-10 rounded-xl">
         <p className=" text-left text-sm lg:text-lg p-5 text-[#bdbdbd]">
-          I'm a dedicated Full-Stack Developer with a passion for creating
+          I&apos;m a dedicated Full-Stack Developer with a passion for creating
           reliable and scalable web applications using Python, Django,
           JavaScript, and React. I enjoy solving problems and have a passion for
-          taking on challenging projects. I'm skilled in both back-end and
+          taking on challenging projects. I&apos;m skilled in both back-end and
           front-end development, ensuring smooth user experiences. I value
           writing clean, maintainable code and am always eager to learn and grow
           in the ever-evolving world of web development. Plus, I love being part
